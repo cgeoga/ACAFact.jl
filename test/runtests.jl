c74fe9fe224963ce8334fede324f0617ecbdb6a8
Chris Geoga <geoga@wisc.edu>
2024-01-04 15:54:17 -0600

    Add complex-valued matrix test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,14 @@ pts1 = range(0.0, 1.0, length=100)
 pts2 = range(0.0, 1.0, length=110)
 K    = [exp(-abs2(pj - pk)) for pj in pts1, pk in pts2]
 
-@testset begin "out-of-place"
+@testset "out-of-place" begin
   (U1, V1) = aca(K, 50)
   @test opnorm(K - U1*V1') < 1e-14
   (U2, V2) = aca(K, 50, tol=1e-6)
   @test opnorm(K - U2*V2') < 1e-5
 end
 
-@testset begin "in-place and resume"
+@testset "in-place and resume" begin
   (U3, V3)  = (zeros(length(pts1), 50), zeros(length(pts2), 50))
   cache     = ACAFact.ACACache(Float64, length(pts1), length(pts2), 50)
   (rank, z) = ACAFact.aca!(K, U3, V3, 1e-6, cache=cache)
@@ -23,5 +23,16 @@ end
   @test U3 ≈ U5
   @test V3 ≈ V5
   @test opnorm(K - U3*V3') < 1e-14
+end
+
+@testset "complex-valued" begin
+  K_im = [exp(-im*abs2(pj - pk)) for pj in pts1, pk in pts2] + K
+  (U_im, V_im) = aca(K_im, 50)
+  K_im_rec = U_im*V_im'
+  # here is something spooky: if you replace K_im_rec with U_im*V_im' (its
+  # value), you get something completely different and incorrect and the opnorm
+  # of the difference is like 100. I am kind of at a loss as to why that
+  # happens.
+  @test opnorm(K_im - K_im_rec) < 1e-13
 end
 
